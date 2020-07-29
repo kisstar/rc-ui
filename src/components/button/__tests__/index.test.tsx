ae68a138d150fa13d252f3fd42750959f087f606
kisstar <dwh.chn@foxmail.com>
2020-07-29 23:42:54 +0800

    test(button): test the basic functions

---
+++ src/components/button/__tests__/index.test.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Button from '..';
+
+describe('Button', () => {
+  it('renders correctly', () => {
+    expect(<Button>Follow</Button>).toMatchSnapshot();
+  });
+
+  it('should add appropriate classes according to different props', () => {
+    const type = 'primary';
+    const size = 'large';
+    const customClass = 'custom-class';
+    const wrapper = render(
+      <Button danger type={type} size={size} className={customClass}>
+        Button
+      </Button>,
+    );
+    const element = wrapper.getByText('Button');
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass(`btn btn-danger btn-${type} btn-${size} ${customClass}`);
+  });
+
+  it('should render as link when href is defined', () => {
+    const wrapper = render(<Button href="https://kisstar.github.io/">Link</Button>);
+    const element = wrapper.getByText('Link');
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual('A');
+  });
+
+  it('should not clickable when button is disabled', () => {
+    const handler = jest.fn();
+    const wrapper = render(
+      <Button onClick={handler} disabled>
+        Button
+      </Button>,
+    );
+    const element = wrapper.getByText('Button');
+    expect(element).toBeInTheDocument();
+    expect(element).toBeDisabled();
+    fireEvent(
+      element,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }),
+    );
+    expect(handler).not.toHaveBeenCalled();
+  });
+
+  it('should update and unmount without errors', () => {
+    const wrapper = render(<Button>Button</Button>);
+    expect(() => {
+      wrapper.rerender(<Button>Button</Button>);
+      wrapper.unmount();
+    }).not.toThrow();
+  });
+});
