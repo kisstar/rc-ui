60d36db4c9d694e58b72f9fbfafef720787eab5c
kisstar <dwh.chn@foxmail.com>
2020-08-15 18:50:46 +0800

    chore: install the correct type declaration for the test library

--- src/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/react';
-
-declare module '@testing-library/react' {
-  // eslint-disable-next-line import/prefer-default-export
-  export const fireEvent: any;
-  export const wait: any;
-}
