fcce5fbe344dea4ea2f80d9f6c3b2d17e4908a60
kisstar <dwh.chn@foxmail.com>
2020-08-16 18:17:45 +0800

    Updates

---
+++ precache-manifest.9a40a4c7f8a814f126202dfec906e6d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e1830aa7683d1377540a652b2253ff",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.415bf2113eb3afc5c607.bundle.js"
+  },
+  {
+    "url": "runtime~main.415bf2113eb3afc5c607.bundle.js"
+  },
+  {
+    "revision": "277504f556f9f4d7430e",
+    "url": "static/css/main.286c9fc9.chunk.css"
+  },
+  {
+    "revision": "65f7a7797e5ded4c4ece",
+    "url": "static/css/vendors~main.8ff17cee.chunk.css"
+  },
+  {
+    "url": "vendors~main.415bf2113eb3afc5c607.bundle.js"
+  },
+  {
+    "url": "vendors~main.415bf2113eb3afc5c607.bundle.js.LICENSE.txt"
+  }
+]);
